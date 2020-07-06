314ec9b282d0b1891dbff6b502fab5cdf236d75f
ebonynon <sithumsandeepap@gmail.com>
2020-07-06 18:40:02 +0530

    part schema:0

---
+++ models/Part.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const PartSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+  },
+  modle: {
+    type: String,
+    required: true,
+  },
+  applicability: {
+    type: String,
+    required: true,
+  },
+  part_number: {
+    type: String,
+    required: true,
+  },
+  production_period: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String,
+    required: true,
+  },
+  base_price: {
+    type: String,
+    required: true,
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Part = mongoose.model("part", PartSchema);
