3fff0224f893a1e382d65a7dcbf15b64e8ffe002
ebonynon <sithumsandeepap@gmail.com>
2020-07-16 13:35:32 +0530

    add cust:0

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var cors = require("cors");
 const userRouter = require("./routes/user"); //auth
 //const books = require("./routes/api/books");
 const parts = require("./routes/api/parts"); //parts
+const customers = require("./routes/api/customers");
 
 const app = express();
 
@@ -24,12 +25,15 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Welcome to Anginz! \n github.com/ebonynon"));
+app.get("/", (req, res) =>
+  res.send("Welcome to Anginz! \n github.com/ebonynon")
+);
 app.get("/users", (req, res) => res.send("He.. user! \n github.com/ebonynon"));
 
 // use Routes
 //app.use("/api/books", books);
 app.use("/api/parts", parts); //parts
+app.use("/api/customers", customers);
 app.use("/users", userRouter);
 
 const port = process.env.PORT || 8082;
